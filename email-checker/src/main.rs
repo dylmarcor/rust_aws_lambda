c8097c13cc194a48182eb034de487c047c1e5f03
dylmarcor <dylancorbett@icloud.com>
2020-04-05 11:58:55 -0700

    add rust email-checker logic

--- email-checker/src/main.rs
+++ email-checker/src/main.rs
@@ -1,3 +1,31 @@
+// My version of Julien Blanchard's email checker from
+// http://julienblanchard.com/2015/rust-on-aws-lambda/
+
+extern crate regex;
+use regex::Regex;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("Starting email-checker...");
+
+    // Create an email regex
+    let re = Regex::new(r"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+)*$").unwrap();
+
+    // Match the first arg against the regex
+    match env::args().nth(1) {
+        // We have an argument
+        Some(email) => {
+            if re.is_match(&email) {
+                println!("{} is a valid email.", email);
+            } else {
+                println!("{} is NOT a valid email.", email);
+            }
+        }
+
+        // No argument provided
+        None => {
+            println!("Please provide a string to test.");
+            return;
+        }
+    };
+};
